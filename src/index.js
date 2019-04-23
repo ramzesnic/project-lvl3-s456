1005d23b152affea56e3dd0dc33317a1ebef9ecd
Roman <ramzesnic@gmail.com>
2019-04-23 17:30:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { promises as fs } from 'fs';
 import axios from 'axios';
 import path from 'path';
-import url from 'url';
 
 const saveFile = (pageURL, data, dir) => {
   const dirName = path.normalize(dir);
