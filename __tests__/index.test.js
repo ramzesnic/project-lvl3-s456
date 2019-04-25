50b63844c7942f6ab395744161aad2952c339b76
Roman <ramzesnic@gmail.com>
2019-04-25 19:35:44 +0300

    add resouce loader

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,10 +12,17 @@ const host = 'https://hexlet.io';
 axios.defaults.host = host;
 axios.defaults.adapter = httpAdapter;
 
-const originalPagePath = path.join(__dirname, '__fixtures__', 'original_index.html');
+const fixtures = '__fixtures__';
+const originalPagePath = path.join(__dirname, fixtures, 'original_index.html');
+const modifiedPagePath = path.join(__dirname, fixtures, 'modified_index.html');
+const resources = {
+  img: path.join(__dirname, fixtures, 'files/img.jpg'),
+  script: path.join(__dirname, fixtures, 'files/script.js'),
+  style: path.join(__dirname, fixtures, 'files/style.css'),
+};
 
 test('download page', async () => {
-  const pathName = '/test';
+  const pathName = '/download-test';
   const testData = await fs.readFile(originalPagePath, 'utf-8');
   const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), '__download-test'));
   const httpPath = url.resolve(host, pathName);
@@ -30,4 +37,44 @@ test('download page', async () => {
   const localPath = path.join(tempDir, files.find(fileName => path.extname(fileName) === '.html'));
   const data = await fs.readFile(localPath, 'utf-8');
   expect(data).toBe(testData);
+});
+
+test('download resources', async () => {
+  const pathName = '/download-resouce-test';
+  const localData = {
+    html: await fs.readFile(modifiedPagePath, 'utf-8'),
+    img: await fs.readFile(resources.img, 'utf-8'),
+    script: await fs.readFile(resources.script, 'utf-8'),
+    style: await fs.readFile(resources.style, 'utf-8'),
+  };
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), '__download-resouces-test'));
+  const resourcesDir = path.join(tempDir, 'hexlet-io-download-resouce-test_files');
+  const httpPath = url.resolve(host, pathName);
+
+  nock(host)
+    .get(pathName)
+    .reply(200, localData.html)
+    .get('/files/img.jpg')
+    .reply(200, localData.img)
+    .get('/files/script.js')
+    .reply(200, localData.script)
+    .get('/files/style.css')
+    .reply(200, localData.style);
+
+  await pageLoader(httpPath, tempDir);
+
+  const files = await fs.readdir(resourcesDir);
+  const resourcesPath = {
+    img: path.join(resourcesDir, files.find(fileName => path.extname(fileName) === '.jpg')),
+    script: path.join(resourcesDir, files.find(fileName => path.extname(fileName) === '.js')),
+    style: path.join(resourcesDir, files.find(fileName => path.extname(fileName) === '.css')),
+  };
+  const data = {
+    img: await fs.readFile(resourcesPath.img, 'utf-8'),
+    script: await fs.readFile(resourcesPath.script, 'utf-8'),
+    style: await fs.readFile(resourcesPath.style, 'utf-8'),
+  };
+  expect(data.img).toBe(localData.img);
+  expect(data.script).toBe(localData.script);
+  expect(data.style).toBe(localData.style);
 });
--- src/index.js
+++ src/index.js
@@ -1,22 +1,73 @@
 import { promises as fs } from 'fs';
 import axios from 'axios';
 import path from 'path';
+import url from 'url';
+import cheerio from 'cheerio';
 
-const saveFile = (pageURL, data, dir) => {
+const tagType = {
+  link: 'href',
+  img: 'src',
+  script: 'src',
+};
+
+const getLocalPath = (pageURL, dir) => {
   const dirName = path.normalize(dir);
   const fileName = path.join(pageURL.host, pageURL.pathname).replace(/\W/g, '-');
-  const fullPath = path.join(dirName, fileName.concat('.html'));
-  return fs.writeFile(fullPath, data).catch((error) => {
-    throw new Error(error);
-  });
+  return path.join(dirName, fileName);
+};
+
+const formatLocalFileName = fileName => fileName.replace(/\//g, '-');
+
+const getDomWithLocalUrls = (dom, dir, baseUrl) => {
+  const links = dom(Object.keys(tagType).join(','))
+    .filter((i, el) => {
+      const node = dom(el);
+      const [{ name }] = node.get();
+      const urlType = tagType[name];
+      const fileUrl = node.attr(urlType);
+      const { hostname } = url.parse(fileUrl);
+      return !hostname;
+    })
+    .map((i, el) => {
+      const node = dom(el);
+      const [{ name }] = node.get();
+      const urlType = tagType[name];
+      const remoteUrl = new URL(node.attr(urlType), baseUrl).href;
+      const formatedUrl = formatLocalFileName(node.attr(urlType));
+      const localUrl = path.join(dir, formatedUrl);
+
+      dom(el).attr(urlType, localUrl);
+      return { remoteUrl, localUrl };
+    });
+  return { dom, links };
+};
+
+const saveResources = (links) => {
+  const urls = links.reduce((acc, e) => ({ ...acc, [e.remoteUrl]: e.localUrl }), {});
+  const fnPromise = Object.keys(urls).map(link => axios.get(link, { responseType: 'arraybufer' })
+    .then(response => fs.writeFile(urls[link], response.data)));
+  return Promise.all(fnPromise);
 };
 
 export default (urlPage, dir) => {
   const pageURL = new URL(urlPage);
+  const basePath = getLocalPath(pageURL, dir);
+  const baseUrl = `${pageURL.protocol}//${pageURL.host}`;
+  const htmlPath = basePath.concat('.html');
+  const localResDir = basePath.concat('_files');
+  let resLinks;
 
   return axios.get(pageURL.href)
-    .then(response => saveFile(pageURL, response.data, dir))
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const { dom: newDom, links } = getDomWithLocalUrls($, localResDir, baseUrl);
+      resLinks = links.map((i, el) => el).get();
+
+      return fs.writeFile(htmlPath, newDom.html());
+    })
+    .then(() => fs.mkdir(localResDir))
+    .then(() => saveResources(resLinks))
     .catch((error) => {
-      throw new Error(error);
+      throw error;
     });
 };
