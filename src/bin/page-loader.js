0a5cc1749e029026c57f833c49370a7c7976a853
Roman <ramzesnic@gmail.com>
2019-04-26 17:20:23 +0300

    fix load data files

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -7,7 +7,7 @@ commander
   .description('Page loader')
   .arguments('<urlPage>')
   .option('-v, --version', 'output the version number')
-  .option('-o, --output [path]', 'output path', __dirname)
+  .option('-o, --output [path]', 'output path', process.cwd())
   .action((urlPage) => {
     pageLoader(urlPage, commander.output)
       .then(() => console.log('OK'))
--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ const tagType = {
   script: 'src',
 };
 
-const getLocalPath = (pageURL, dir) => {
-  const dirName = path.normalize(dir);
-  const fileName = path.join(pageURL.host, pageURL.pathname).replace(/\W/g, '-');
-  return path.join(dirName, fileName);
+const getLocalPath = (pageURL) => {
+  const { host, pathname } = pageURL;
+  const fileName = path.join(host, pathname).replace(/\W/g, '-');
+  return fileName;
 };
+
+// const testfunc = text => console.log(text);
 
 const formatLocalFileName = fileName => fileName.replace(/\//g, '-');
 
@@ -25,7 +27,11 @@ const getDomWithLocalUrls = (dom, dir, baseUrl) => {
       const [{ name }] = node.get();
       const urlType = tagType[name];
       const fileUrl = node.attr(urlType);
+      if (!fileUrl) {
+        return false;
+      }
       const { hostname } = url.parse(fileUrl);
+
       return !hostname;
     })
     .map((i, el) => {
@@ -42,31 +48,32 @@ const getDomWithLocalUrls = (dom, dir, baseUrl) => {
   return { dom, links };
 };
 
-const saveResources = (links) => {
+const saveResources = (links, dir) => {
   const urls = links.reduce((acc, e) => ({ ...acc, [e.remoteUrl]: e.localUrl }), {});
-  const fnPromise = Object.keys(urls).map(link => axios.get(link, { responseType: 'arraybufer' })
-    .then(response => fs.writeFile(urls[link], response.data)));
+  const fnPromise = Object.keys(urls).map(link => axios.get(link, { responseType: 'arraybuffer' })
+    .then(response => fs.writeFile(path.join(dir, urls[link]), response.data)));
   return Promise.all(fnPromise);
 };
 
 export default (urlPage, dir) => {
   const pageURL = new URL(urlPage);
-  const basePath = getLocalPath(pageURL, dir);
+  const dirName = path.normalize(dir);
+  const formatedUrl = getLocalPath(pageURL);
+  const basePath = path.join(dirName, formatedUrl);
   const baseUrl = `${pageURL.protocol}//${pageURL.host}`;
   const htmlPath = basePath.concat('.html');
-  const localResDir = basePath.concat('_files');
   let resLinks;
 
   return axios.get(pageURL.href)
     .then((response) => {
       const $ = cheerio.load(response.data);
-      const { dom: newDom, links } = getDomWithLocalUrls($, localResDir, baseUrl);
+      const { dom: newDom, links } = getDomWithLocalUrls($, formatedUrl.concat('_files'), baseUrl);
       resLinks = links.map((i, el) => el).get();
 
       return fs.writeFile(htmlPath, newDom.html());
     })
-    .then(() => fs.mkdir(localResDir))
-    .then(() => saveResources(resLinks))
+    .then(() => fs.mkdir(basePath.concat('_files')))
+    .then(() => saveResources(resLinks, dirName))
     .catch((error) => {
       throw error;
     });
